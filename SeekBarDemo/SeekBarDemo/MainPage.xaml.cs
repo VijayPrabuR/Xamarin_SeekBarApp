54420c46297e28b1501cbf60bd68a65377f73817
VijayPrabuR <83070020+VijayPrabuR@users.noreply.github.com>
2021-04-23 16:56:46 +0530

    Add files via upload

---
+++ SeekBarDemo/SeekBarDemo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SeekBarDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnSeekBarChanged(object sender, ValueChangedEventArgs args)
+        {
+            if (sender == redSeekBar)
+            {
+                redText.Text = String.Format("{0:X2}", (int)args.NewValue);
+            }
+            else if (sender == greenSeekBar)
+            {
+                greenText.Text = String.Format("{0:X2}", (int)args.NewValue);
+            }
+            else if (sender == blueSeekBar)
+            {
+                blueText.Text = String.Format("{0:X2}", (int)args.NewValue);
+            }
+
+            boxView.Color = Color.FromRgb((int)redSeekBar.Value,(int)greenSeekBar.Value,(int)blueSeekBar.Value);
+        }
+    }
+}
